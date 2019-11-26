0457fd8612682637748fffeb56e8e106a2536768
ratneshn <ratneshnagarjun@gmail.com>
2019-11-26 12:19:33 -0500

    Update textAnalysis.py

--- textAnalysis.py
+++ textAnalysis.py
@@ -186,15 +186,14 @@ class Content:
 		word = word.lower()
 		count = 0
 		vowels = "aeiouy"
-		if word[0] in vowels:
-			count += 1
+		count += 1 if word[0] in vowels
+
 		for index in range(1, len(word)):
-			if word[index] in vowels and word[index - 1] not in vowels:
-				count += 1
-		if word.endswith("e"):
-			count -= 1
-		if count == 0:
-			count += 1
+			count += 1 if word[index] in vowels and word[index - 1] not in vowels
+				
+		count -= 1 if word.endswith("e")	
+		count += 1 if count == 0:
+			
 		return count
 
 	def clean_content(self, text=None, punctuations=True, digits=True, custom_regex=None):
